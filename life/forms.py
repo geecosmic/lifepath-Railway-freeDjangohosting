d4b0066006dda6f079de6b184224811b3bfacb49
George <georgedredd@gmail.com>
2025-10-17 13:51:31 +0100

    second commit

--- life/forms.py
+++ life/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class DateConversionForm(forms.Form):
     gregorian_date = forms.DateField(widget=forms.TextInput(attrs={
         'type': 'date',
-        'placeholder': 'Select a date',
+        'placeholder': 'Select a date', 
     }))
